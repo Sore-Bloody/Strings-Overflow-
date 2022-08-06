d2962880bb79e1c6085df989c12b835d428c0693
Yaga۶Lan <109000323+Sore-Bloody@users.noreply.github.com>
2022-08-06 14:08:17 +0200

    Create PilaDeString.py
    Signed-off-by: Yaga۶Lan <109000323+Sore-Bloody@users.noreply.github.com>

---
+++ PilaDeString.py
@@ -0,0 +1,45 @@
+#FALTA POR AÑADIR LA FUNCION DE CALL
+
+
+
+
+stack  = 0
+while (stack < 16): stack  += 1; print("A") or print("B") or print("C") or print("D")
+
+
+
+#Push: Add an element to the stack                                                                                    #Pop: Remove an element from the stack
+#Peek: Get the topmost element of the stack
+
+
+
+
+
+sft = []                                                                                                              sft.append('A' * 16)
+sft.append('B' * 16)
+sft.append('C' * 16)
+sft.append('D' * 16)
+
+print ("push: se añade a la variable  sft", sft)
+
+ret = []
+ret.append('A' * 16)
+ret.append('B' * 16)
+ret.append('C' * 16)
+ret.append('D' * 16)
+print ("push: Se añade a la variable  ret", ret)
+
+
+
+
+a = []
+
+a.append('A' * 16)
+a.append('B' * 16)
+a.append('C' * 16)
+a.append('D' * 16)
+
+print ("push: Se añade a la variable a", ret)
+
+
+print ("Se han añadido un tamaño de 64 bits al STACK")
